745d8f5db1af116d971567af51d58219e5a9c4a5
Robert Witt <robert.witt@zalando.de>
2019-01-05 05:26:55 +0100

    Implement alert in POActionController to enter additional note when approving.

--- Approve Purchase Orders/POActionController.swift
+++ Approve Purchase Orders/POActionController.swift
@@ -12,13 +12,25 @@ import UIKit
 class POActionController {
     
     let viewController: UIViewController
+    private var noteTextField: UITextField?
     
     init(viewController: UIViewController) {
         self.viewController = viewController
     }
     
-    func approvePurchaseOrder(_ purchaseOrder: PurchaseOrder, completion: (Bool, Error?) -> Void) {
-        // TODO: Invoke note alert and call backend
+    func approvePurchaseOrder(_ purchaseOrder: PurchaseOrder, completion: @escaping (Bool, Error?) -> Void) {
+        let alert = UIAlertController(title: NSLocalizedString("noteAlertTitle", comment: ""), message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            self.noteTextField = textField
+            self.noteTextField?.placeholder = NSLocalizedString("noteAlertPlaceholderOptional", comment: "")
+        }
+        alert.addAction(UIAlertAction(title: NSLocalizedString("noteAlertButtonCancel", comment: ""), style: .cancel, handler: { (action) in
+            completion(false, nil)
+        }))
+        alert.addAction(UIAlertAction(title: NSLocalizedString("noteAlertButtonOK", comment: ""), style: .default, handler: { (action) in
+            // TODO: Invoke backend
+        }))
+        viewController.present(alert, animated: true)
     }
     
     func rejectPurchaseOrder(_ purchaseOrder: PurchaseOrder, completion: (Bool, Error?) -> Void) {
