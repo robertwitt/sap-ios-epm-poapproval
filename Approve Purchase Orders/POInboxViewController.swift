9f5387c0facd07458d0fc7a1f626cc51fa8b751e
Robert Witt <robert.witt@zalando.de>
2019-01-05 02:21:11 +0100

    Add POInboxViewController with static sample table data.

---
+++ Approve Purchase Orders/POInboxViewController.swift
@@ -0,0 +1,56 @@
+//
+//  POInboxViewController.swift
+//  Approve Purchase Orders
+//
+//  Created by Robert Witt on 05.01.19.
+//  Copyright © 2019 SAP. All rights reserved.
+//
+
+import UIKit
+import SAPFiori
+
+class POInboxViewController: UITableViewController {
+    
+    // MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(FUIObjectTableViewCell.self, forCellReuseIdentifier: FUIObjectTableViewCell.reuseIdentifier)
+        tableView.estimatedRowHeight = 80.0
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+
+    // MARK: Table View Data Source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FUIObjectTableViewCell.reuseIdentifier, for: indexPath) as! FUIObjectTableViewCell
+        cell.headlineText = "Panorama Studios"
+        cell.subheadlineText = "Ordered by Peter Fuchs"
+        cell.footnoteText = "2 days ago"
+        cell.statusText = "6.196,26"
+        cell.substatusText = "USD"
+        cell.accessoryType = .disclosureIndicator
+
+        return cell
+    }
+
+    /*
+    // MARK: Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
