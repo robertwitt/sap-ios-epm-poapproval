f55051dfa35e8a7fbddf13ce04fa595b7430a59a
Robert Witt <robert.witt@zalando.de>
2019-01-05 13:32:29 +0100

    Extract preparation of note alert in POActionController into separate method.

--- Approve Purchase Orders/POActionController.swift
+++ Approve Purchase Orders/POActionController.swift
@@ -12,29 +12,42 @@ import UIKit
 class POActionController {
     
     let viewController: UIViewController
-    private var noteTextField: UITextField?
+    private var noteAlert: UIAlertController?
     
     init(viewController: UIViewController) {
         self.viewController = viewController
     }
     
     func approvePurchaseOrder(_ purchaseOrder: PurchaseOrder, completion: @escaping (Bool, Error?) -> Void) {
-        let alert = UIAlertController(title: NSLocalizedString("noteAlertTitle", comment: ""), message: nil, preferredStyle: .alert)
-        alert.addTextField { (textField) in
-            self.noteTextField = textField
-            self.noteTextField?.placeholder = NSLocalizedString("noteAlertPlaceholderOptional", comment: "")
-        }
-        alert.addAction(UIAlertAction(title: NSLocalizedString("noteAlertButtonCancel", comment: ""), style: .cancel, handler: { (action) in
+        prepareNoteAlert(mandatory: false, cancelHandler: { (action) in
             completion(false, nil)
-        }))
-        alert.addAction(UIAlertAction(title: NSLocalizedString("noteAlertButtonOK", comment: ""), style: .default, handler: { (action) in
+        }, okHandler: { (action) in
             // TODO: Invoke backend
-        }))
-        viewController.present(alert, animated: true)
+        })
+        viewController.present(noteAlert!, animated: true)
     }
     
-    func rejectPurchaseOrder(_ purchaseOrder: PurchaseOrder, completion: (Bool, Error?) -> Void) {
-        // TODO: Invoke note alert and call backend
+    func rejectPurchaseOrder(_ purchaseOrder: PurchaseOrder, completion: @escaping (Bool, Error?) -> Void) {
+        prepareNoteAlert(mandatory: true, cancelHandler: { (action) in
+            completion(false, nil)
+        }, okHandler: { (action) in
+            // TODO: Invoke backend
+        })
+        viewController.present(noteAlert!, animated: true)
+    }
+    
+    private func prepareNoteAlert(mandatory: Bool, cancelHandler: @escaping (UIAlertAction) -> Void, okHandler: @escaping (UIAlertAction) -> Void) {
+        let alert = UIAlertController(title: NSLocalizedString("noteAlertTitle", comment: ""), message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = mandatory ? NSLocalizedString("noteAlertPlaceholderMandatory", comment: "") : NSLocalizedString("noteAlertPlaceholderOptional", comment: "")
+        }
+        alert.addAction(UIAlertAction(title: NSLocalizedString("noteAlertButtonCancel", comment: ""), style: .cancel, handler: cancelHandler))
+        
+        let okAction = UIAlertAction(title: NSLocalizedString("noteAlertButtonOK", comment: ""), style: .default, handler: okHandler)
+        okAction.isEnabled = !mandatory
+        alert.addAction(okAction)
+        
+        noteAlert = alert
     }
     
 }
