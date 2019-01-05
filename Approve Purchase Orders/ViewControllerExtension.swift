e43d3ff9ff6dee5505ca90e728d1c51922595468
Robert Witt <robert.witt@zalando.de>
2019-01-05 01:43:06 +0100

    Add strings file for localization and view controller extensions with generic functions.

---
+++ Approve Purchase Orders/ViewControllerExtension.swift
@@ -0,0 +1,25 @@
+//
+//  ViewControllerExtension.swift
+//  Approve Purchase Orders
+//
+//  Created by Robert Witt on 05.01.19.
+//  Copyright © 2019 SAP. All rights reserved.
+//
+
+import UIKit
+import SAPOData
+
+extension UIViewController {
+    
+    private var dataService: EPMREFAPPSPOAPVSRVEntities<OnlineODataProvider> {
+        return (UIApplication.shared.delegate as! AppDelegate).epmrefappspoapvsrvEntities
+    }
+    
+    private func showAlert(withError error: Error) {
+        let alert = UIAlertController(title: NSLocalizedString("errorAlertTitle", comment: ""),
+                                      message: error.localizedDescription,
+                                      preferredStyle: .alert)
+        self.present(alert, animated: true)
+    }
+    
+}
